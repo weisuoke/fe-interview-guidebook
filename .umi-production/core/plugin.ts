f452ce42431142597d5ba66c68b7401fc374b720
梅林的法杖 <530264127@qq.com>
2020-09-30 20:52:49 +0800

    remove git cache

--- .umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/wuxiao/wuxiao-desktop/weisuoke/guidebook/fe-interview-guidebook/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','ssr',],
-});
-
-export { plugin };
